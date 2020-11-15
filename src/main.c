accc70171a407e14a4af0f96e7763feb3a5e287c
Amir Ramazanov <70631226+Ramazanov-Amir@users.noreply.github.com>
2020-11-15 12:34:15 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,12 +5,14 @@ void other()
 {
     printf("!!!!!!!!!Attention!!!!!!!!!\n"
            " ***********!hacker attack!***********\n");
+    fflush(stdout);
     exit(0);
 }
 
 void input()
 {
     char buffer[5];
+    fflush(stdout);
     gets(buffer);
 }
 
@@ -18,6 +20,7 @@ int main()
 {
     printf("%p", &other);
     input();
+    printf("well\n");
     fflush(stdout);
     return 0;
 }
